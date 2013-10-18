a3d832876b4283af2bbd836358e750bb68202ef1
Donato Rotunno <donato.rotunno@liip.ch>
2013-10-19 00:10:11 +0200

    Initial commit

---
+++ src/Lightgear/Asset/AssetManager.php
@@ -0,0 +1,23 @@
+<?php namespace Lightgear\Asset;
+
+class AssetManager {
+
+    protected $stylesheets = array();
+
+    protected $scripts = array();
+
+    public function stylesheets()
+    {
+
+    }
+
+    public function scripts()
+    {
+
+    }
+
+    protected function add($assets, $group = '')
+    {
+        
+    }
+}
---
+++ src/Lightgear/Asset/AssetServiceProvider.php
@@ -0,0 +1,38 @@
+<?php namespace Lightgear\Asset;
+
+use Illuminate\Support\ServiceProvider;
+
+class AssetServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->package('lightgear/asset');
+
+		$this->app['asset'] = $this->app->share(function($app) {
+			return new AssetManager($app);
+		});
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
