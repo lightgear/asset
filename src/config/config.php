cb548d8893e8b34e3f05895a40c098c85cbe592a
Donato Rotunno <donato.rotunno@liip.ch>
2013-10-27 00:12:33 +0200

    Add forgotten config directory

---
+++ src/config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'base_paths' => array(
+        base_path() . '/workbench',
+        base_path() . '/vendor',
+        base_path() . '/app/assets',
+    ),
+    'combine' => false,
+    'minify' => false,
+);
