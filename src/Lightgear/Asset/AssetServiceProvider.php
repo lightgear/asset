eda23ed7c8dddc3420d287f3f4dd1adf423a7221
Graham Campbell <graham@mineuk.com>
2013-11-30 19:42:03 +0000

    Formatting fixes

--- src/Lightgear/Asset/AssetServiceProvider.php
+++ src/Lightgear/Asset/AssetServiceProvider.php
@@ -4,56 +4,56 @@ use Illuminate\Support\ServiceProvider;
 
 class AssetServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('lightgear/asset');
+    /**
+     * Bootstrap the application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('lightgear/asset');
     }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['asset'] = $this->app->share(function($app)
-		{
-			return new Asset($app);
-		});
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['asset'] = $this->app->share(function($app)
+        {
+            return new Asset($app);
+        });
 
-		$this->app['asset.generate'] = $this->app->share(function($app)
-		{
-			return new Commands\Generate;
-		});
+        $this->app['asset.generate'] = $this->app->share(function($app)
+        {
+            return new Commands\Generate;
+        });
 
-		$this->app['asset.clean'] = $this->app->share(function($app)
-		{
-			return new Commands\Clean;
-		});
+        $this->app['asset.clean'] = $this->app->share(function($app)
+        {
+            return new Commands\Clean;
+        });
 
-		$this->commands('asset.generate');
-		$this->commands('asset.clean');
-	}
+        $this->commands('asset.generate');
+        $this->commands('asset.clean');
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 }
